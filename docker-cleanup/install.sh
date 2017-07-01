89aad03612c4016294e6b91415c2993afa0f71e1
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2017-07-01 13:55:27 -0300

    add bash to docker-cleanup image

--- docker-cleanup/install.sh
+++ docker-cleanup/install.sh
@@ -12,6 +12,9 @@ cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 # install docker
 apk add docker
 
+# install bash
+apk add bash
+
 # install go-cron
 apk add curl
 curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
--- docker-cleanup/run.sh
+++ docker-cleanup/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "${SCHEDULE}" = "**None**" ]; then
-  sh cleanup.sh
+  bash cleanup.sh
 else
   exec go-cron "$SCHEDULE" /bin/sh cleanup.sh
 fi
